78dc67c72213814482dcdebd8a659d1bf3ab177a
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-16 12:37:13 +0000

    categorical feature encoding with numpy
    replaced the categorical feature encoding of scikit.learn by my own personal implementation in numpy

--- classification/knn/dating_matches.py
+++ classification/knn/dating_matches.py
@@ -14,9 +14,24 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, len(dataset.columns)-1].values
 
 # data preprocessing
-from sklearn.preprocessing import LabelEncoder
-encoder = LabelEncoder()
-y = encoder.fit_transform(y)
+
+def encode_categorical(categorical_feature):
+    """
+    Encode labels of one categorical feature with value between 0 and n_categories-1.
+    :type categorical_feature: numpy.ndarray
+    :param categorical_feature: feature contanining different categories
+    :return: numpy.ndarray with categories replaced by integers
+    """
+    categories = list(np.unique(categorical_feature))
+    numerical_feature = np.zeros(categorical_feature.shape)
+    num_instances = categorical_feature.shape[0]
+    for i in range(num_instances):
+        num_label = categories.index(categorical_feature[i])
+        numerical_feature[i] = num_label
+    
+    return numerical_feature
+    
+y = encode_categorical(y)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()

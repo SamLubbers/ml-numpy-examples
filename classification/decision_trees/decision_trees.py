2f6e07e92ba0cfd170c56a9692eff16ab5696548
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-22 16:47:43 +0000

    implemented function to figure out the best feature by which to split the dataset

--- classification/decision_trees/decision_trees.py
+++ classification/decision_trees/decision_trees.py
@@ -2,8 +2,7 @@ from math import log
 
 
 def calculate_entropy(dataset):
-    """
-    calculates the entropy of the given dataset
+    """calculates the entropy of the given dataset
     :param dataset: pandas.DataFrame
     :return: entropy of the given dataset
     """
@@ -22,8 +21,8 @@ def calculate_entropy(dataset):
     return entropy
 
 def split_dataset(dataset, feature):
-    """
-    splits the dataset into subsets according to the specified feature.
+    """splits the dataset into subsets according to the specified feature.
+
     the number of subsets created equals the number of unique values for the given feature,
     with each subset being a collection of instances that share same value for the given feature
     :param dataset: pandas.DataFrame representing the dataset
@@ -37,3 +36,34 @@ def split_dataset(dataset, feature):
         subsets.append(dataset.loc[dataset[feature] == value])
         
     return subsets
+
+def best_split_feature(dataset):
+    """identifies the best feature by which to split the dataset for a decision tree
+
+    the best feature is chosen according to the ID3 algorithm,
+    which states that the best feature is the one with the maximum information gain
+    :param dataset: pandas.DataFrame representing the dataset
+    :return: feature by which to split the dataset, which will be the feature with the highest information gain
+    """
+    base_entropy = calculate_entropy(dataset)
+    max_information_gain = 0.0
+    best_feature = ''
+
+    total_instances = dataset.shape[0]
+    iv_subdataset = dataset.iloc[:, :-1] # subset of dataset with only independent variables
+    for feature in iv_subdataset:
+        subdatasets = split_dataset(dataset, feature)
+
+        new_entropy = 0.0
+        for subdataset in subdatasets:
+            subdataset_instances = subdataset.shape[0]
+            prob = subdataset_instances/total_instances
+            new_entropy += (prob * calculate_entropy(subdataset))
+
+        information_gain = base_entropy - new_entropy
+        if information_gain > max_information_gain:
+            max_information_gain = information_gain
+            best_feature = feature
+
+    return best_feature
+

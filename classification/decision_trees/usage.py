0db4bc12a78b9305e0243d0bd7eaa058f79aed46
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-22 16:16:22 +0000

    implemented usage example for split_dataset funciton

--- classification/decision_trees/usage.py
+++ classification/decision_trees/usage.py
@@ -3,7 +3,7 @@ example usage of the decision_tree module
 """
 
 # create dataset
-from decision_trees import calculate_entropy
+from decision_trees import calculate_entropy, split_dataset
 import pandas as pd
 from collections import OrderedDict
 
@@ -15,4 +15,11 @@ dataset = pd.DataFrame(data_dict)
 
 # calculate the entropy for our target variable
 entropy = calculate_entropy(dataset)
-print('entropy of the dataset is %f' % entropy)
+print('entropy of the dataset is %f\n' % entropy)
+
+# split dataset according to the first feature
+split_feature = 'v1'
+subsets = split_dataset(dataset, split_feature)
+print('the subsets of splitting the dataset by feature %s are:' % split_feature)
+for subset in subsets:
+    print(subset)

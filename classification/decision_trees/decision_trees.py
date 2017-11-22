890785753502438b2a6db04e81a27ac1923e76a5
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-22 16:15:36 +0000

    implemented split_dataset function

--- classification/decision_trees/decision_trees.py
+++ classification/decision_trees/decision_trees.py
@@ -20,3 +20,20 @@ def calculate_entropy(dataset):
         entropy -= prob * log(prob, 2)
     
     return entropy
+
+def split_dataset(dataset, feature):
+    """
+    splits the dataset into subsets according to the specified feature.
+    the number of subsets created equals the number of unique values for the given feature,
+    with each subset being a collection of instances that share same value for the given feature
+    :param dataset: pandas.DataFrame representing the dataset
+    :param feature: name of the feature by which we want to split the dataset
+    :return: list of subsets of the datasets, each subset of type pandas.DataFrame
+    """ 
+    subsets = []
+    feature_unique_values= dataset[feature].unique()
+    
+    for value in feature_unique_values:
+        subsets.append(dataset.loc[dataset[feature] == value])
+        
+    return subsets

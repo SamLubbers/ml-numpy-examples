9628f5fdd81553d6143e10748917eacb89ac8534
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-23 15:50:20 +0000

    implemented functions to recursively calculate tree depth and width

--- classification/decision_trees/tree_plot.py
+++ classification/decision_trees/tree_plot.py
@@ -5,7 +5,6 @@ decision_node = dict(boxstyle="sawtooth", fc="0.8")
 leaf_node = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
-
 def plot_node(node_txt, point_coordinates, text_coordinates, node_type):
     create_plot.ax1.annotate(node_txt,
                  xy=point_coordinates,
@@ -16,6 +15,28 @@ def plot_node(node_txt, point_coordinates, text_coordinates, node_type):
                  ha='center',
                  bbox=node_type,
                  arrowprops=arrow_args)
+
+def calculate_tree_leaves(my_tree):
+    num_leaves = 0
+    subnodes = next(iter(my_tree.values()))
+    for subnode in subnodes.values():
+        if type(subnode) is dict:
+            num_leaves += calculate_tree_leaves(subnode)
+        else:
+            num_leaves += 1
+    return num_leaves
+    
+def calcualte_tree_depth(my_tree):
+    max_depth = 0
+    subnodes = next(iter(my_tree.values()))
+    for subnode in subnodes.values():
+        if type(subnode) is dict:
+            depth = 1 + calcualte_tree_depth(subnode)
+        else:
+            depth = 1
+        if depth > max_depth:
+            max_depth = depth
+    return max_depth
 
 def create_plot():
     fig = plt.figure(1, facecolor="white")
@@ -30,5 +51,3 @@ def create_plot():
           text_coordinates=(0.8, 0.1),
           node_type=leaf_node)
     plt.show()
-    
-create_plot()

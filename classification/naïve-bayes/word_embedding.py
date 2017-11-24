ea588287498fc2f802fc9f9b3de418b6c4f5c332
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-24 19:02:22 +0000

    implemented function that converts text onto a vector of words

--- classification/naïve-bayes/word_embedding.py
+++ classification/naïve-bayes/word_embedding.py
@@ -14,8 +14,7 @@ raw_data = [OrderedDict({"review":"the food was incredible","positive":1}),
 dataset = pd.DataFrame(raw_data)
 
 def create_vocabulary(documents):
-    """
-    creates a list of all the words contained in a set of documents
+    """creates a list of all the words contained in a set of documents
     :type documents: numpy.ndarray or list of strings
     :param documents: documents can be a set of reviews, comments, tweets...
     :return: list of the individual words that appear in the documents
@@ -28,3 +27,19 @@ def create_vocabulary(documents):
     return list(vocabulary_set)
 
 vocabulary = create_vocabulary(dataset['review'].values)
+
+def word2vector(vocab_list, text):
+    """converts a text onto a vector with vocabulary words as features
+    with value 1 if word occurs in text and 0 if it doesn't
+    :param vocab_list: set of words in our vocabulary
+    :param text: text we want to convert to a vector
+    :return: vector representing text
+    """
+    word_vec = []
+    text = text.split()
+    for word in vocab_list:
+        if word in text:
+            word_vec.append(1)
+        else:
+            word_vec.append(0)
+    return word_vec

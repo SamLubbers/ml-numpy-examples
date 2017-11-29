c928236c06eabb46dcd6deb61345097f7486247c
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-29 15:22:28 +0000

    linear regression decision boundary

---
+++ classification/logistic_regression/plot.py
@@ -0,0 +1,19 @@
+from usage import dataset, weights
+import matplotlib.pyplot as plt
+import numpy as np
+
+# separate instnaces by labels
+class_0_points = dataset.loc[dataset['label'] == 0].iloc[:, [1, 2]].values
+class_1_points = dataset.loc[dataset['label'] == 1].iloc[:, [1, 2]].values
+
+# plot data points
+plt.scatter(class_0_points[:, 0], class_0_points[:, 1], c='red', marker='s')
+plt.scatter(class_1_points[:, 0], class_1_points[:, 1], c='blue')
+
+# plot decision boundary
+x = np.arange(min(dataset.iloc[:, 1].values), max(dataset.iloc[:, 1].values), 0.1)
+weights = weights.getA()
+# we equal input to sigmoid function to 0 and solve for x2
+x2 = (-weights[0]-weights[1]*x)/weights[2]
+plt.plot(x, x2)
+plt.show()
--- classification/logistic_regression/usage.py
+++ classification/logistic_regression/usage.py
@@ -4,8 +4,15 @@
 import pandas as pd
 dataset = pd.read_csv('testSet.txt', delimiter='\t', header=None)
 
+dataset.columns = ['x1', 'x2', 'label']
+
+# we add a column of ones by convention
+import numpy as np
+dataset.insert(0, 'x0', np.ones(len(dataset)))
+
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1:].values
+
 
 # calculate optimal weights
 from logistic_regression import optimal_weights

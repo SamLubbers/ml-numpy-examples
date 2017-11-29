af4d9c879cd0d9dd92541b74f5d90ea8a746e98d
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-29 16:59:08 +0000

    plot stochastic gradient descent decision boundary

--- classification/logistic_regression/plot.py
+++ classification/logistic_regression/plot.py
@@ -1,4 +1,4 @@
-from usage import dataset, weights
+from usage import dataset, weights, weights_stochastic
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,9 +10,14 @@ class_1_points = dataset.loc[dataset['label'] == 1].iloc[:, [1, 2]].values
 plt.scatter(class_0_points[:, 0], class_0_points[:, 1], c='red', marker='s')
 plt.scatter(class_1_points[:, 0], class_1_points[:, 1], c='blue')
 
-# plot decision boundary
+# plot decision boundary - gradient descent
 x = np.arange(min(dataset.iloc[:, 1].values), max(dataset.iloc[:, 1].values), 0.1)
 # we equal input to sigmoid function to 0 and solve for x2
 x2 = (-weights[0]-weights[1]*x)/weights[2]
 plt.plot(x, x2)
+
+# plot decision boundary - stochastic gradient descent
+x2_s = (-weights_stochastic[0]-weights_stochastic[1]*x)/weights_stochastic[2]
+plt.plot(x, x2_s)
+
 plt.show()

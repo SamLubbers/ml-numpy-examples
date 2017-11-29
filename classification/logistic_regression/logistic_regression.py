fixed stochastic gradient descent random instance selection

classification/logistic_regression/logistic_regression.py
@@ -42,13 +42,14 @@ def optimal_weights_stochastic_ascent(data, labels, num_iter=150):
     m, n = data.shape
     weights = np.ones(n)  # vector containing one weight for each feature
     for j in range(num_iter):
-        unaccessed_indexes = range(m)
+        unaccessed_indexes = list(range(m))
         for i in range(m):
             alpha = alpha_start_value/(1.0+j+i) + alpha_constant # step size decreases as iterations increase
             random_index = int(random.uniform(0, len(unaccessed_indexes)))
             prediction = sigmoid(sum(data[random_index]*weights))
             error_rate = (labels[random_index] - prediction)
             weights = weights + alpha * data[random_index] * error_rate
+            del(unaccessed_indexes[random_index])
 
     return weights
 
c78806d55daf18f725f6c2d2c1e6c81ef64c2003
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-03 12:43:44 +0000

    calculate new alpha_j value

--- classification/support_vector_machines/smo.py
+++ classification/support_vector_machines/smo.py
@@ -84,26 +84,44 @@ def smo_simple(data, labels, C, tolerance, max_iterations):
     alphas = np.zeros((m, 1))
     bias = 0
     # run optimization
+    min_alpha_change = 0.00001
     iteration = 0
     while(iteration<max_iterations):
         alpha_pairs_changed = 0
         for i in range(m):
             error_i = instance_error_rate(i, data_matrix, labels_matrix, alphas, bias)
             if can_be_optimized(alphas[i], labels_matrix[i], error_i, C, tolerance):
-                # if alpha can be optimized get another instance at random
+
+                # if alpha can be optimized get another instance at random and calculate its error rate
                 j = random_index(i, m)
                 error_j = instance_error_rate(j, data_matrix, labels_matrix, alphas, bias)
-                # make sure alpha stays between 0 and C
+
+                #  make sure alpha stays between 0 and C
                 min_limit, max_limit = define_limits(labels_matrix[i],
                                                      labels_matrix[j],
                                                      alphas[i],
                                                      alphas[j],
                                                      C)
-                if min_limit == max_limit:
-                    continue # loop over to the next instance as alpha values for this instance are bound
+
+                # if alpha values for this instance are bound move onto next instance
+                if min_limit == max_limit: continue
+
                 # calculate optimal amount by which to change alpha_j
                 ocf = optimal_change_factor(instance_i=data_matrix[i, :], instance_j=data_matrix[j, :])
-                
+
+                # move onto next instance if optimal change factor is 0
+                # this is an oversimplification of the original smo algorithm
+                if ocf >= 0: continue
+
+                # calculate new alpha value of j
+                alpha_j_old = alphas[j].copy()
+                alphas[j] -= labels_matrix[j] * (error_i - error_j) / ocf
+                alphas[j] = bound_alpha(alphas[j], max_limit, min_limit)
+
+                # if change in alpha is not notable move onto next instances
+                alpha_j_new = alphas[j]
+                if abs(alpha_j_new - alpha_j_old) < min_alpha_change: continue
+
 
         if alpha_pairs_changed == 0:
             iteration += 1

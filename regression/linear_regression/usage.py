82aeed14718361ee29eb768bf51b3a02d53d490b
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-07 08:03:21 +0000

    calculated regression weights on test dataset

---
+++ regression/linear_regression/usage.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# loading dataset
+dataset = pd.read_csv('ex0.txt', delimiter='\t', header=None)
+
+dataset.columns = ['offset', 'x', 'y']
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1:].values
+
+from linear_regression import calculate_regression_weights
+
+weights = calculate_regression_weights(X, y)

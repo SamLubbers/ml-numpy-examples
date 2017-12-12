c205447523d96cd4ba6e6217703ee5e0e72201fa
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-12 08:17:40 +0000

    plotted how multiple ridge regression weights change with different lambda values

--- regression/linear_regression/plot.py
+++ regression/linear_regression/plot.py
@@ -1,13 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from usage import X, y, X_sorted, y_hat, y_hat_lwlr
+from usage import X, y, X_sorted, y_hat, y_hat_lwlr, abalone_multiple_weights
 
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
 # scatter data points
-plt.scatter(X[:, 1], y, color='gray')
+ax.scatter(X[:, 1], y, color='gray')
 
 # plot linear regression line
-plt.plot(X_sorted[:, 1], y_hat, color='blue')
+ax.plot(X_sorted[:, 1], y_hat, color='blue')
 
 # plot local weighted linear regression line
-plt.plot(X_sorted[:, 1], y_hat_lwlr, color='red')
+ax.plot(X_sorted[:, 1], y_hat_lwlr, color='red')
+
+# plotting multiple coefficients obtained through ridge regression
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.plot(abalone_multiple_weights)
+
 plt.show()

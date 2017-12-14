f004a9cfffb40ce81d03f73f62a04ab53b78147f
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-14 11:17:34 +0000

    implemented kmeans clustering algorithm

--- unsupervised_learning/k-means-clustering/kmeans.py
+++ unsupervised_learning/k-means-clustering/kmeans.py
@@ -21,3 +21,40 @@ def random_centroids(dataset, k):
 def euclidean_distance(vec_a, vec_b):
     """calculates the euclidean distance between 2 vectors"""
     return np.sqrt(np.sum(np.power(vec_a - vec_b, 2)))
+
+def kmeans(dataset, k, initialize_centroids=random_centroids, calculate_distance=euclidean_distance):
+    """clustering of instances in dataset into k clusters
+
+    :type dataset: numpy.ndarray (mxn)
+    :param dataset: data on which we will perform the clustering
+    :param k: number of clusters
+    :param initialize_centroids: function used to initialize centroids
+    :param calculate_distance: function used to calculate distance between centroids and points
+    :return: matrix of cluster assigned to each instance along with its associated error
+    """
+    num_instances = dataset.shape[0]
+    cluster_assignment = np.mat(np.zeros((num_instances, 2)))
+    centroids = initialize_centroids(dataset, k=k)
+
+    clusters_changed = True
+    while clusters_changed:
+        clusters_changed = False
+        # assign each point to the cluster with the closest centroid
+        for i, instance in enumerate(dataset):
+            closest_cluster = -1
+            min_distance = np.inf # minimum calculate_distance from instance to centroid
+            for cluster, centroid in enumerate(centroids):
+                distance = calculate_distance(instance, centroid)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_cluster = cluster
+            if cluster_assignment[i, 0] != closest_cluster: clusters_changed = True
+            cluster_assignment[i, 0] = closest_cluster
+            error = min_distance ** 2
+            cluster_assignment[i, 1] = error
+        # update centroids
+        for cluster, _ in enumerate(centroids):
+            cluster_instances = dataset[np.nonzero(cluster_assignment[:, 0].A == cluster)[0]]
+            centroids[cluster, :] = np.mean(cluster_instances, axis=0)
+
+    return centroids, cluster_assignment
--- unsupervised_learning/k-means-clustering/usage.py
+++ unsupervised_learning/k-means-clustering/usage.py
@@ -6,8 +6,12 @@ dataset = pd.read_csv('testSet.txt', delimiter='\t', header=None).values
 
 # initialize random centroids on the dataset
 from kmeans import random_centroids
-centroids = random_centroids(dataset, k=2)
+rand_centroids = random_centroids(dataset, k=2)
 
 # euclidean distance from point to centroid
 from kmeans import euclidean_distance
-distance = euclidean_distance(dataset[0,:], centroids[0, :])
+distance = euclidean_distance(dataset[0,:], rand_centroids[0, :])
+
+# kmeans clustering of dataset
+from kmeans import kmeans
+centroids, cluster_assignments = kmeans(dataset, k=4)

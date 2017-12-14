b109044e816ca3c15dbd40da429ebf0d33b83d70
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-14 18:00:32 +0000

    filter out itemsets that do not have a minimum support

--- unsupervised_learning/apriori/apriori.py
+++ unsupervised_learning/apriori/apriori.py
@@ -9,4 +9,25 @@ def create_initial_itemsets(dataset):
     """
     initial_itemsets = [item for transaction in dataset for item in transaction]
     initial_itemsets.sort()
-    return set(initial_itemsets)
+    initial_itemsets = set(initial_itemsets)
+    initial_itemsets = [[item] for item in initial_itemsets]
+    return list(map(frozenset, initial_itemsets)) # map to frozen set so we can use itemsets as keys for dicts
+
+def filter_itemsets(dataset, itemsets, min_support):
+    """filters our from itemsets each itemset with a support lower than min_support"""
+    itemset_count = {}
+    dataset = list(map(set, dataset)) # convert each transaction to set to eliminate duplicates
+    for transaction in dataset:
+            for itemset in itemsets:
+            if itemset.issubset(transaction):
+                itemset_count[itemset] = itemset_count.setdefault(itemset, 0) + 1
+
+    num_items = len(dataset)
+    filtered_itemsets = []
+    itemests_support = {}
+    for itemset, count in itemset_count.items():
+        support = count / num_items
+        if support >= min_support:
+            filtered_itemsets.append(itemset)
+        itemests_support[itemset] = support
+    return filtered_itemsets, itemests_support
--- unsupervised_learning/apriori/usage.py
+++ unsupervised_learning/apriori/usage.py
@@ -3,5 +3,9 @@
 # create fictional dataset of transactions
 dataset = [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
 
+
 from apriori import create_initial_itemsets
 initial_itemsets = create_initial_itemsets(dataset)
+
+from apriori import filter_itemsets
+filtered_itemsets, itemsets_support = filter_itemsets(dataset, initial_itemsets, min_support=0.5)

66b7b033b7b6d77602e1b65c7db0557819242e33
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-19 08:20:26 +0000

    sort transaction items by frequency of occurance

--- unsupervised_learning/FP-growth/fpgrowth.py
+++ unsupervised_learning/FP-growth/fpgrowth.py
@@ -1,4 +1,5 @@
 """FP-growth algorithm. Uses an FP-tree data structure to quickly find most frequent itemsets"""
+import operator
 
 class Node(object):
     """node of an fp-tree"""
@@ -23,7 +24,10 @@ def prepare_input_data(dataset):
     return data
 
 def create_header_table(dataset, min_support):
-    """creates the header table, necessary for the creation of the fp-tree"""
+    """creates the header table, necessary for the creation of the fp-tree
+
+    :param dataset: dictionary with frozensets of instances as keys and their frequency as values
+    """
     header_table = {}
     for transaction in dataset:
         for item in transaction:
@@ -37,3 +41,30 @@ def create_header_table(dataset, min_support):
         header_table[item] = {'count': header_table[item], 'pointer': None}
 
     return header_table
+
+def create_tree(dataset, header_table):
+    """creates the FP-tree given a dataset of transaction and a header_table for that dataset
+
+    :param dataset: dictionary with frozensets of instances as keys and their frequency as values
+    :param header_table: dictionary containing the frequency of each item, without pointers to nodes
+    :return: FP-tree and header_table, with pointers to nodes
+    """
+    # tree main node
+    tree = Node('Null set', 1, None)
+
+    # sort items of transactions by relevance and populate node
+    frequent_items = set(header_table.keys())
+    for transaction, count in dataset.items():
+        # each transaction is converted to a dict that holds the frequency of each item in that transaction
+        transaction_items_count = {}
+        for item in transaction:
+            if item in frequent_items:
+                transaction_items_count[item] = header_table[item]['count']
+
+        if transaction_items_count: # only proceed if transaction_items_count is not empty
+            # sort items in transaction by relevance
+            ordered_transaction = [item_count[0] for item_count in sorted(transaction_items_count.items(),
+                                                                          key=operator.itemgetter(1),
+                                                                          reverse=True)]
+
+    return tree, header_table

c17ac446ff5322e28baf822256ec1c0e226b2cb4
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-21 16:20:53 +0100

    principal component variance percentage and cumulative variance percentage

--- dimensionality_reduction/principal_component_analysis/pca.py
+++ dimensionality_reduction/principal_component_analysis/pca.py
@@ -6,16 +6,33 @@ def pca(dataset, n_features=3):
 
     it will return a dataset with n_features number of features or as many as the original dataset has
     :type dataset: numpy.matrix (mxn)
-    :param n_features: number of features of the new dataset
+    :param n_features: number of principal components (number of features of the new dataset)
     :return: dataset with best extracted features, and reconstructed dataset using those features
     """
     mean = np.mean(dataset, axis=0)
     dataset_distance_mean = dataset - mean
     covariance_matrix = np.cov(dataset_distance_mean, rowvar=False)
     eigenvalues, eigenvectors = np.linalg.eig(np.mat(covariance_matrix))
+    # get eigenvectors with largest eigenvalues
     eigenvalues_indexes = np.argsort(eigenvalues)
-    eigenvalues_indexes = eigenvalues_indexes[: -(n_features + 1): -1] # sort and trim list of eigenvalues
+    eigenvalues_indexes = eigenvalues_indexes[: -(n_features + 1): -1]
     top_eigenvectors = eigenvectors[:, eigenvalues_indexes]
+    # use these eigenvectors to create new dataset with principal components
     new_dataset = dataset_distance_mean * top_eigenvectors
     reconstructed_dataset = (new_dataset * top_eigenvectors.T) + mean
+    # reconstruct dataset using principal component
     return np.array(new_dataset), np.array(reconstructed_dataset)
+
+def principal_component_variance(dataset):
+    """calculates the percentage of variance each principal component explains
+
+    :type dataset: numpy.matrix (mxn)
+    :param n_features: number of features of the new dataset
+    :return: list containing the percentage of variance each principal component explains
+    """
+    mean = np.mean(dataset, axis=0)
+    dataset_distance_mean = dataset - mean
+    covariance_matrix = np.cov(dataset_distance_mean, rowvar=False)
+    eigenvalues, eigenvectors = np.linalg.eig(np.mat(covariance_matrix))
+    variance_percentage = [((eigenvalue/sum(eigenvalues)) * 100) for eigenvalue in eigenvalues]
+    return variance_percentage
